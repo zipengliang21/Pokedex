516824c5a9714e5fde0eadee9993cd4439a1d440
Zipeng(Jepson) <zipengliang21@gmail.com>
2021-06-03 21:55:02 -0700

    Rough Work for Left Nav Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import {Switch, Route} from "react-router-dom";
+import NavBar from "./components/NavBar";
+import styled from "styled-components";
+
+const Test = styled.span`
+  position: fixed;
+  left: 300px;
+  top: 0;
+`;
 
 function App() {
   return (
       <Switch>
          <Route exact path="/">
-            <div className="App">
-               Pokemon - CPSC455 Project
-            </div>
+            <NavBar />
+            <Test>Home</Test>
+         </Route>
+         <Route exact path="/profile">
+            <NavBar />
+            <Test>Profile</Test>
+         </Route>
+         <Route exact path="/filterSearch">
+            <NavBar />
+            <Test>Filter Search</Test>
+         </Route>
+         <Route exact path="/pokemonComments">
+            <NavBar />
+            <Test>Pokemon Comments</Test>
          </Route>
       </Switch>
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {HashRouter as Router} from "react-router-dom";
+import './index.scss';
 import App from './App';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHome, faAddressCard, faSearch, faComment, faAngleUp} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faHome, faAddressCard, faSearch, faComment, faAngleUp)
 
 ReactDOM.render(
     <Router>
