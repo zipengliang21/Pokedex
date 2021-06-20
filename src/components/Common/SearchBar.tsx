ee9befd25bbefaaaaab8e22218caca81beb87806
Zipeng(Jepson) <zipengliang21@gmail.com>
2021-06-20 16:01:57 -0700

    Update Home Page

--- src/components/Common/SearchBar.tsx
+++ src/components/Common/SearchBar.tsx
@@ -1,50 +1,91 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from "react";
 import styled from "styled-components";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 
 
-const SearchBarDiv = styled.div`
+const SearchBarWrapper = styled.div`
+  display: flex;
+  background: #616161;
+  mix-blend-mode: normal;
   color: blue;
-  form, input, button{
-    margin-left: 10px;
+  padding: 20px 0;
+  .left{
+    text-align: left;
+    margin-left: 80px;
+    .title{
+      font-size: 20px;
+      color: #04266D;
+      font-weight: bold;
+    }
+    .note{
+      margin: 10px 0;
+      width: 280px;
+      font-size: 14px;
+      color: #FFFFFF;
+    }
   }
+  .right{
+    display: flex;
+    width: 400px;
+    margin: auto;
+    padding: 10px 0;
+    background: #59B937;
+    color: #FFFFFF;
+    border-radius: 10px;
+    align-items: center;
+    text-align: center;
+  }
+  .searchButton{
+    color: white;
+    background: linear-gradient(90deg, rgba(116, 230, 154, 0.72) 0%, rgba(139, 212, 212, 0.920305) 51.43%);
+    border-radius: 5px;
+    margin-left: 10px;
+    padding: 4px;
+    border: none;
+  }
+  #search{
+    border-radius: 5px;
+    height: 28px;
+    border: 3px solid #04266D;
+  }
+ 
   @media(max-width: 576px){
     form, input, button{
       margin-top: 15px;
       margin-left: 0;
     }
   }
-`
+`;
 
-function SearchBar (props:any){
-    const [Key, setKey] = useState("");
+function SearchBar(props: any) {
+   const [Key, setKey] = useState("");
 
-    const handleSubmit = (event:any) => {
-        setKey(event.target.value);
-    };
-    return (
-        <SearchBarDiv>
-            {/*action="/" method="get"*/}
-            <form>
-                <label htmlFor="header-search">
-                    <FontAwesomeIcon icon="search"/>
-                </label>
+   const handleSubmit = (event: any) => {
+      setKey(event.target.value);
+   };
+   return (
+       <SearchBarWrapper>
+          <div className="left">
+             <div className="title">Name or Number</div>
+             <form>
                 <input
                     type="text"
-                    id="header-search"
+                    id="search"
                     placeholder="Search Pokemon names"
                     name="s"
                     onChange={(e) => setKey(e.target.value)}
                 />
-                <Button variant="contained" color="primary" onClick={handleSubmit}>
-                    Search
-                </Button>
-            </form>
-        </SearchBarDiv>
-    )
+                <button onClick={handleSubmit} className="searchButton">
+                   <FontAwesomeIcon icon="search"/>
+                </button>
+             </form>
+             <div className="note">Use the Advanced Search to explore Pokemon by different filters</div>
+          </div>
+          <div className="right">Search for a Pokemon by name or using its National Pokedex number.</div>
+       </SearchBarWrapper>
+   );
 }
-
 
 
 export default SearchBar;
--- src/views/WelcomePage.tsx
+++ src/views/WelcomePage.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import styled from "styled-components";
-import {NavLink} from "react-router-dom";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import SearchBar from "../components/Common/SearchBar";
 import PokemonData from "../pokedexData.json";
 import Pokemon from "../components/DetailedView/Pokemon";
 import {Button} from "../components/Button";
 import Fade from "react-reveal/Fade";
+import NavBar from "../components/Common/NavBar";
+import Header from "../components/Common/Header";
 
 const Background = styled.div`
-      //background: #A1BDC6;
   background: url("https://onlyvectorbackgrounds.com/wp-content/uploads/2019/03/Subtle-Lines-Abstract-Gradient-Background-Cool.jpg")
   no-repeat center center fixed rgba(161, 189, 198, 0.75);
   background-size: cover;
@@ -25,7 +24,7 @@ const Wrapper = styled.div`
         text-align: center;
       }
       .loadMore{
-        margin-bottom: 50px;
+        margin: 50px auto;
       }
       @media(max-width: 875px){
         width: 418px;
@@ -39,47 +38,6 @@ const Wrapper = styled.div`
           margin-top: 25px;
         }
       }
-`;
-
-const DecorationBar = styled.div`
-  width: 750px;
-  height: 320px;
-  margin: 0 auto;
-  background-image: url("https://www.nintendo.com.au/web_images/pages/1939/attachments/attach1473204999.png");
-  margin-bottom: 40px;
-  @media(max-width: 875px){
-   width: 418px;
-   display: none;
-  }
-`;
-
-const Header = styled.div`
-   display: flex;
-   justify-content: space-between;
-   font-size: 16px;
-   font-weight: 600;
-   color: rgba(71, 32, 121, 0.9);
-   height: 40px;
-   align-items: center;
-   background: linear-gradient(90deg, rgba(177, 209, 202, 0.83) 20.21%, rgba(178, 223, 217, 0.79) 60.83%, #B2E6DF 78.85%);
-   .home{
-       margin: 0 5px 0 40px
-   }
-   .login{
-       margin-right: 50px;
-   }
-   .user{
-      margin-right: 5px;
-   }
-   @media(max-width: 576px){
-      justify-content: center;
-      .home{
-        margin: 0 5px 0 0;
-      }
-      .login{
-       margin: 0 0 0 5px;
-      }
-   }
 `;
 
 const PokemonWrapper = styled.div`
@@ -110,20 +68,9 @@ const WelcomePage = () => {
    return (
        <Background>
           <Wrapper>
-             <Header>
-                <NavLink exact activeClassName="selected" to="/" className="home">
-                   <FontAwesomeIcon icon="home" className="home"/>
-                   <span className="span">Home</span>
-                </NavLink>
-                <NavLink exact activeClassName="selected" to="/login" className="login">
-                   <FontAwesomeIcon icon="user" className="user"/>
-                   <span className="span">Login</span>
-                </NavLink>
-             </Header>
-             <DecorationBar/>
-             <div className="search">
-                <SearchBar/>
-             </div>
+             <Header/>
+             <NavBar/>
+             <SearchBar className="search"/>
              <PokemonWrapper>
                 <GroupWrapper>
                    {PokemonData.slice(0, 4).map((pokemon, index) => {
