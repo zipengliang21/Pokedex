8784c304d035e6393f149b2e375e8378d30599e8
Zipeng(Jepson) <zipengliang21@gmail.com>
2021-07-21 09:41:22 -0700

    Include Menu Dropdown in Header

--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import styled from "styled-components";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
@@ -18,37 +17,74 @@ const HeaderWrapper = styled.div`
    font-family: 'Poppins', sans-serif;
    color: rgba(71, 32, 121, 0.9);
    height: 40px;
-   background: linear-gradient(90deg, rgba(177, 209, 202, 0.83) 20.21%, rgba(178, 223, 217, 0.79) 60.83%, #B2E6DF 78.85%);
-   .login{
+   background: linear-gradient(90.07deg, rgba(177, 209, 202, 0.83) 0.07%, rgba(207, 233, 229, 0.79) 99.95%);
+   #login{
        margin-right: 50px;
-   }
-   .user{
-      margin-right: 5px;
    }
    @media(max-width: 875px){
       width: 418px;
-      .login{
+      #login{
           margin-right: 20px;
       }
    }
    @media(max-width: 576px){
       width: 100%;
       justify-content: center;
-      .login{
+      #login{
        margin: 0 0 0 5px;
       }
    }
 `;
 
+const DropDown = styled.div`
+  .user{
+      margin-right: 5px;
+  }
+  .dropdown-content {
+     display: none;
+     position: absolute;
+     background-color: rgba(207, 233, 229);
+     min-width: 80px;
+     box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+     z-index: 1;
+     .logout{
+        border-top: 1px solid rgba(71, 32, 121, 0.9);
+     }
+  }
+  .dropdown-content a {
+     color: rgba(71, 32, 121, 0.9);
+     font-size: 14px;
+     font-weight: normal;
+     padding: 12px 16px;
+     display: block;
+     text-align: left;
+  }
+  :hover .dropdown-content{
+     display: block;
+  }
+  .dropdown-content a:hover {
+     background-color: #B2E6DF;
+  }
+`;
+
+const Logout = () => {
+   console.log("Logout")
+}
+
 function Header (props: any){
-   console.log(props.currentUser)
    let value = props.currentUser? props.currentUser.userName: "Login";
    return (
        <HeaderWrapper>
-          <NavLink exact activeClassName="selected" to="/login" className="login">
-             <FontAwesomeIcon icon="user" className="user"/>
-             <span className="span">{value}</span>
-          </NavLink>
+          <DropDown className="dropdown" id="login">
+             <NavLink exact to="/login">
+                <FontAwesomeIcon icon="user" className="user"/>
+                <span className="span">{value}</span>
+             </NavLink>
+             <div className="dropdown-content">
+                <NavLink exact to="/profile">Profile</NavLink>
+                <a className="logout" onClick={() => Logout()} >Logout</a>
+             </div>
+          </DropDown>
        </HeaderWrapper>
    )
 }
